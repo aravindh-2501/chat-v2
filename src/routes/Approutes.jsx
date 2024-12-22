4d474048c7e81b280b96d03b34965470c16d0942
aravindh-2501 <muthuaravindh@iaaxin.com>
2024-12-22 16:39:41 +0530

    updated routes and theme

--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -1,35 +1,45 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Register from "../auth/Register";
 import Login from "../auth/Login";
 import Chat from "../pages/chat";
 import ErrorBoundary from "../utils/ErrorBoundary";
+import { useUserStore } from "../store/userStore";
 
-const AppRoutes = () => (
-  <Routes>
-    {/* Default Route (with Error Boundary) */}
-    <Route
-      path="/"
-      element={
-        <ErrorBoundary>
-          <Chat />
-        </ErrorBoundary>
-      }
-    />
+const AppRoutes = () => {
+  const user = useUserStore((state) => state.currentUser);
 
-    {/* Chat Route with User ID as URL Parameter */}
-    <Route
-      path="/chat/:id"
-      element={
-        <ErrorBoundary>
-          <Chat />
-        </ErrorBoundary>
-      }
-    />
+  console.log("");
 
-    {/* Login and Register Routes */}
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
-  </Routes>
-);
+  return (
+    <Routes>
+      {/* Default Route (with Error Boundary) */}
+      <Route
+        path="/"
+        element={
+          user ? (
+            <ErrorBoundary>
+              <Chat />
+            </ErrorBoundary>
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
 
+      {/* Chat Route with User ID as URL Parameter */}
+      <Route
+        path="/chat/:id"
+        element={
+          <ErrorBoundary>
+            <Chat />
+          </ErrorBoundary>
+        }
+      />
+
+      {/* Login and Register Routes */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+};
 export default AppRoutes;
--- src/store/useTheme.js
+++ src/store/useTheme.js
@@ -1,11 +1,14 @@
-import {create} from "zustand";
+import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 export const useTheme = create(
   persist(
     (set) => ({
-      theme: "sunset",
-      setTheme: (theme) => set({ theme }),
+      theme: localStorage.getItem("themeStorage")?.theme || "sunset",
+      setTheme: (theme) => {
+        set({ theme });
+        localStorage.setItem("themeStorage", JSON.stringify({ theme }));
+      },
     }),
     {
       name: "themeStorage",
